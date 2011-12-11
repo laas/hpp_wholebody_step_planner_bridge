d07513d5d5f467af0f07e67a6651a0fad71ccc15
Antonio El Khoury <aelkhour@laas.fr>
2011-12-11 18:07:54 +0100

    Create a planner and generate a whole-body trajectory.

--- src/trajectory-writer.cc
+++ src/trajectory-writer.cc
@@ -1,8 +1,113 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+#include <KineoModel/kppUserData.h>
+#include <KineoWorks2/kwsUtility.h>
+#include <KineoModel/kppLicense.h>
+
+#include <kwsIO/kwsioPath.h>
+#include <kwsIO/kwsioConfig.h>
+#include <hpp/wholebody-step-planner/planner.hh>
+
+#include "hpp/wholebody_step_planner/bridge/pattern-generator.hh"
+
+#include "load-hrp2.cc"
+
+int main(int argc, char *argv[])
 {
-  std::cout << "left-ankle-trajectory: [0., 0., 0.]\n"
-	    << std::endl;
+  using namespace hpp::wholeBodyStepPlanner;
+
+  // Check arguments number. Should be 1 if only path is given, and 2 if environment and path (in that order) are given.
+  // if (argc != 2 && argc != 3)
+  //   {
+  //     std::cerr	<< "Wrong arguments given, expected 1 (path to path.kxml) or 2 (path to env.kxml and path.kxml)" << std::endl;
+  //     return -1;
+  //   }
+
+  // Check license.
+  if ( !CkppLicense::initialize() )
+    {
+      std::cerr << "Error: license for KPP SDK was not found" << std::endl;
+      return -1;
+    }
+  else
+    {
+      std::cout << "license for KPP SDK was found" << std::endl;
+    }
+
+  CkppUserData::getInstance()->initializeParameters();
+  CkwsUtility::randomSeed (time (NULL));
+
+  // Create planner instance and set footprint limits.
+  Planner* planner = new Planner (0.05);
+  
+  planner->setFootPrintLimits(-0.2,0.2,-0.25,-0.13,-M_PI /4,0.1);
+
+  // Build HRP-2 robot and load it in planner.
+  ChppHumanoidRobotShPtr robot;
+  if (loadHrp2Model (robot) !=  KD_OK)
+      std::cerr << "Planner::initScene(): error in loading HRP2"
+		<< std::endl;
+  
+  planner->addHppProblem(robot,0.1);
+
+  // Initialize planner.
+
+  planner->initializeProblem ();
+
+  // Load environment from file given in first argument if two
+  // arguments are given.
+  
+  // if (KD_OK != planner->parseFile (argv[1]))
+  //   {
+  //     std::cerr << "Planner::parseFile: could not load environment."
+  // 		<< std::endl;
+  //     std::cerr << "Make sure that the environment path is correctly set."
+  // 		<< std::endl;
+  //     return -1;
+  //     }
+  
+  // FIXME: It would be better to solve the planning problem rather
+  // than load a valid path.
+
+  // Load path to animate from file given in first or second argument.
+
+  std::vector <CkwsConfigShPtr> configVector;
+
+  CkwsConfigShPtr config1;
+  planner->humanoidRobot ()->getCurrentConfig (config1);
+  configVector.push_back (config1);
+
+  CkwsConfigShPtr config2;
+  planner->humanoidRobot ()->getCurrentConfig (config2);
+  config2->dofValue (0, 1.5);
+  configVector.push_back (config2);
+
+  CkwsPathShPtr path
+    = CkwsPath::create (planner->humanoidRobot ());
+
+  for (unsigned i = 0; i < configVector.size () - 1; ++i)
+    if (KD_OK !=
+	path->appendDirectPath (configVector[i], configVector[i + 1]))
+      {
+	std::cerr << "Could not append direct path " << i << std::endl;
+	return -1;
+      }
+  
+  std::cout << "number of direct paths: "
+	    << path->countDirectPaths () << std::endl;
+
+  // Remove constraint that constrains feet to the ground before
+  // starting animation.
+
+  planner->humanoidRobot ()->userConstraints ()
+    ->remove (planner->wholeBodyConstraint ());
+
+  // Animate Path.
+
+  planner->findDynamicPath (path);
+
+  delete planner;
+
   return 0;
 }
